41902ba1bee96e53e348d175483f42b8893cac35
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-04-29 14:05:19 +0800

    Initial commit

---
+++ src/Karyawan.java
@@ -0,0 +1,28 @@
+public class Karyawan {
+    String nama;
+    int umur;
+    String pangkat;
+    int gaji;
+    String alamat;
+
+
+    void Karyawan(String nama){
+        nama = nama;
+    }
+
+    void umurKaryawan(int umurKaryawan){
+        umur =  umurKaryawan;
+    }
+
+    void pangkatKaryawan(String pangkatKaryawan){
+        pangkat = pangkatKaryawan;
+    }
+
+    public void gajiKaryawan(int gajiKaryawan){
+        gaji = gajiKaryawan;
+    }
+
+    public void alamat(String Alamat){
+        alamat = Alamat;
+    }
+}
